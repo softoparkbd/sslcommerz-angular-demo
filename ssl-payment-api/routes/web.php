562d89b6d9b729277c18268788388d9a27df190c
Nasirul Islam <nasirul.siat@outlook.com>
2020-10-21 17:03:53 +0600

    added ssl config except .env id's

--- ssl-payment-api/routes/web.php
+++ ssl-payment-api/routes/web.php
@@ -19,3 +19,18 @@ Route::get('/', function () {
 Route::get('/api/items', 'App/Http/Controllers/SslcommerzController@index');
 
 Route::resource('api/items', 'SslcommerzController');
+
+
+// // SSLCOMMERZ Start
+// Route::get('/example1', 'SslCommerzPaymentController@exampleEasyCheckout');
+// Route::get('/example2', 'SslCommerzPaymentController@exampleHostedCheckout');
+
+// Route::post('/pay', 'SslCommerzPaymentController@index');
+// Route::post('/pay-via-ajax', 'SslCommerzPaymentController@payViaAjax');
+
+// Route::post('/success', 'SslCommerzPaymentController@success');
+// Route::post('/fail', 'SslCommerzPaymentController@fail');
+// Route::post('/cancel', 'SslCommerzPaymentController@cancel');
+
+// Route::post('/ipn', 'SslCommerzPaymentController@ipn');
+// //SSLCOMMERZ END
